e1c9c316ac3ec0862d8e685596596c8da8be4387
Luciano Nooijen <lucianonooijen@gmail.com>
2018-06-18 00:16:57 +0200

    Added .env file support

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
+// Set NodeJS process environment variables
+require('dotenv').config();
+
+// Dependency imports
 const express = require('express');
 
-const port = process.env.PORT || 3000;
+// File variables
 const app = express();
+const port = process.env.PORT;
 
-app.get('/', () => {
+app.get('/', (req, res) => {
     res.send('hello world');
-})
+});
 
 app.listen(port, () => {
-    console.log(`Server listening on port ${port}`); // eslint-disable-line
+    console.log(`Server listening on port ${port}`); // eslint-disable-line no-console
 });
