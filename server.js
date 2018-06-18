903e9649abb7fd658b9db50186beb134e9c35fda
Luciano Nooijen <lucianonooijen@gmail.com>
2018-06-18 03:51:06 +0200

    Added controller structure

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 // Set NodeJS process environment variables
 require('dotenv').config();
 
-// Dependency imports
+// NPM Dependency imports
 const express = require('express');
+
+// Project dependency imports
 const middleware = require('./middleware');
+const controllers = require('./controllers');
 
 // File variables
 const app = express();
@@ -12,10 +15,15 @@ const port = process.env.PORT;
 // Register middleware
 middleware.register(app);
 
+// Temp index route
 app.get('/', (req, res) => {
     res.send('hello world');
 });
 
+// Register controllers
+app.use('/', controllers);
+
+// Run Express server
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`); // eslint-disable-line
 });
